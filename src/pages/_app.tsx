c8fbb4100d591fa06a6fe73bb4045b255a06e872
howardt12345 <howardt12345@gmail.com>
2021-12-31 15:48:43 -0500

    Added react hook for when component is mounted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import { AppProps } from "next/app";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ThemeProvider from "@api/ThemeProvider";
 import { GlobalStyle } from "@styles";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <>
-      <ThemeProvider>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <GlobalStyle />
+      {isMounted && <Component {...pageProps} />}
+    </ThemeProvider>
   );
 }
